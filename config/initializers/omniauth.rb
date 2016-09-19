43f987c712300b2f6fa099184eaf7b6375cd22e7
Jason Y <jasonh.yi@gmail.com>
2016-09-19 15:07:59 -0700

    Devise: created model and db migration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
-#OMNI AUTH SETUP FOR GOOGLE API
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, CLIENT_ID, CLIENT_SECRET, {
-    access_type: 'offline',
-    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost/auth/google_oauth2/callback'
-  }
-end
+# #OMNI AUTH SETUP FOR GOOGLE API
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, CLIENT_ID, CLIENT_SECRET, {
+#     access_type: 'offline',
+#     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+#     redirect_uri:'http://localhost/auth/google_oauth2/callback'
+#   }
+# end
